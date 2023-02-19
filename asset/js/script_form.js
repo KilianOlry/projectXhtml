18cd320e5cb9c23605fc82097decc87c484f1aea
Dijoux <chezdijoux@gmail.com>
2023-02-19 22:35:01 +0100

    création formulaire multipage intéractif

--- asset/js/script_form.js
+++ asset/js/script_form.js
@@ -1,45 +1,78 @@
-/*const form = document.getElementById('form'); 
-const progress = document.querySelector('.step')
-const prevBtn = document.querySelectorAll('.prev');
-const nextBtn = document.querySelectorAll('.next');
-const pages = document.querySelectorAll('.page');
-let currentPage = 1;
+var form_1 = document.querySelector(".form_1");
+var form_2 = document.querySelector(".form_2");
+var form_3 = document.querySelector(".form_3");
 
-// Bouton Suivant
-nextBtn.forEach(button => {
-	button.addEventListener('click', () => {
-		currentPage++;
-		if (currentPage > pages.length) {
-			currentPage = pages.length;
-		}
-		updateProgress();
-	});
+
+var form_1_btns = document.querySelector(".form_1_btns");
+var form_2_btns = document.querySelector(".form_2_btns");
+var form_3_btns = document.querySelector(".form_3_btns");
+
+
+var form_1_next_btn = document.querySelector(".form_1_btns .btn_next");
+var form_2_back_btn = document.querySelector(".form_2_btns .btn_back");
+var form_2_next_btn = document.querySelector(".form_2_btns .btn_next");
+var form_3_back_btn = document.querySelector(".form_3_btns .btn_back");
+
+var form_2_progessbar = document.querySelector(".form_2_progessbar");
+var form_3_progessbar = document.querySelector(".form_3_progessbar");
+
+var btn_done = document.querySelector(".btn_done");
+var modal_wrapper = document.querySelector(".modal_wrapper");
+var shadow = document.querySelector(".shadow");
+
+form_1_next_btn.addEventListener("click", function(){
+	form_1.style.display = "none";
+	form_2.style.display = "block";
+
+	form_1_btns.style.display = "none";
+	form_2_btns.style.display = "flex";
+
+	form_2_progessbar.classList.add("active");
 });
 
-// Bouton Précédent
-prevBtn.forEach(button => {
-	button.addEventListener('click', () => {
-		currentPage--;
-		if (currentPage < 1) {
-			currentPage = 1;
-		}
-		updateProgress();
-	});
+form_2_back_btn.addEventListener("click", function(){
+	form_1.style.display = "block";
+	form_2.style.display = "none";
+
+	form_1_btns.style.display = "flex";
+	form_2_btns.style.display = "none";
+
+	form_2_progessbar.classList.remove("active");
 });
 
-// Mise à jour de la barre de progression
-function updateProgress() {
-	progress.style.width = `${(currentPage - 1) / (pages.length - 1) * 100}%`;
-	pages.forEach((page, idx) => {
-		if (idx === currentPage - 1) {
-			page.classList.add('active');
-		} else {
-			page.classList.remove('active');
-		}
-	});
-}
+form_2_next_btn.addEventListener("click", function(){
+	form_2.style.display = "none";
+	form_3.style.display = "block";
 
-//Soumission du formulaire
+	form_3_btns.style.display = "flex";
+	form_2_btns.style.display = "none";
+
+	form_3_progessbar.classList.add("active");
+});
+
+form_3_back_btn.addEventListener("click", function(){
+	form_2.style.display = "block";
+	form_3.style.display = "none";
+
+	form_3_btns.style.display = "none";
+	form_2_btns.style.display = "flex";
+
+	form_3_progessbar.classList.remove("active");
+});
+
+btn_done.addEventListener("click", function(){
+	modal_wrapper.classList.add("active");
+})
+
+shadow.addEventListener("click", function(){
+	modal_wrapper.classList.remove("active");
+})
+
+
+
+
+
+/*Soumission du formulaire
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const formData = new FormData(form);
